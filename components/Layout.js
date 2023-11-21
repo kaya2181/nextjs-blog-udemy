3dd99c26893decb535dd5eb05e1fd50e5a5c2329
kaya2181 <mukkun0124@gmail.com>
2023-11-21 15:06:31 +0900

    all done

---
+++ components/Layout.js
@@ -0,0 +1,47 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+import Head from "next/head";
+import Link from "next/link";
+
+import styles from "./layout.module.css";
+import utilStyles from "../src/styles/utils.module.css";
+
+const name = "Shin Code";
+export const siteTitle = "Next.js blog";
+
+function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img
+              src="/images/profile.png"
+              className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <img
+              src="/images/profile.png"
+              className={`${utilStyles.borderCircle}`}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div>
+          <Link href="/">← ホームへ戻る</Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Layout;
